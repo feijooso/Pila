640bf5c748757ca6578e586c039987d6a10dd7df
sofi <sofeijoo@gmail.com>
2018-09-16 02:36:54 -0300

    correciones en .c y pruebas

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -10,11 +10,12 @@
  *                   PRUEBAS UNITARIAS ALUMNO
  * *****************************************************************/
 
-void pruebas_pila_alumno() {
+void prueba_pila_vacia(){
 
 	printf("PRUEBAS CON PILA VACIA\n");
 
 	pila_t *p = pila_crear();
+
 	print_test("La pila fue creada", p != NULL);
     print_test("La pila está vacía", pila_esta_vacia(p));
     print_test("Intento desapilar", pila_desapilar(p) == NULL);
@@ -22,9 +23,16 @@ void pruebas_pila_alumno() {
     pila_destruir(p);
     print_test("La pila fue destruida", true);
 
-    printf("PRUEBAS DE APILAR\n");
+}
 
-    pila_t *p1 = pila_crear();
+
+
+void prueba_pila_con_elementos(){
+
+	printf("PRUEBAS DE APILAR\n");
+
+	pila_t *p1 = pila_crear();
+
     int a = 2;
     print_test("Se apilo un elemento de tipo int", pila_apilar(p1,&a));
     char b = 'b';
@@ -34,40 +42,52 @@ void pruebas_pila_alumno() {
     print_test("Se apilo un elemento de tipo int", pila_apilar(p1,&a));
     print_test("Se apilo NULL", pila_apilar(p1,NULL));
 
-
-
-
     printf("PRUEBAS DE DESAPILAR\n");
 
-    print_test("Se desapilo el primer elemento", pila_ver_tope(p1) == pila_desapilar(p1));
-    print_test("Se desapilo el segundo elemento", pila_ver_tope(p1) == pila_desapilar(p1));
-    print_test("Se desapilo el tercer elemento", pila_ver_tope(p1) == pila_desapilar(p1));
-    print_test("Se desapilo el cuarto elemento", pila_ver_tope(p1) == pila_desapilar(p1));
-    print_test("Se desapilo el quinto elemento", pila_ver_tope(p1) == pila_desapilar(p1));
-    print_test("Se desapilo el sexto elemento", pila_ver_tope(p1) == pila_desapilar(p1));
+    print_test("Se desapilo el tope", pila_ver_tope(p1) == pila_desapilar(p1));
+    print_test("Se desapilo el tope", pila_ver_tope(p1) == pila_desapilar(p1));
+    print_test("Se desapilo el tope", pila_ver_tope(p1) == pila_desapilar(p1));
+    print_test("Se desapilo el tope", pila_ver_tope(p1) == pila_desapilar(p1));
+    print_test("Se desapilo el tope", pila_ver_tope(p1) == pila_desapilar(p1));
+    print_test("Se desapilo el tope", pila_ver_tope(p1) == pila_desapilar(p1));
     print_test("La pila está vacía", pila_esta_vacia(p1));
     print_test("Intento desapilar", pila_desapilar(p1) == NULL);
     print_test("Intento ver tope", pila_ver_tope(p1) == NULL);
 
 
-    printf("PRUEBAS VOLUMEN\n");
+}
 
-    int* vector = malloc(100 * sizeof(int));
+void pruebas_pila_volumen(int volumen){
 
-    for(int i = 0; i < 100; i++) {
+	printf("PRUEBAS VOLUMEN\n");
+
+    int* vector = malloc(volumen * sizeof(int));
+
+    //pruebo apilar:
+    bool ok = true; 
+    for(int i = 0; i < volumen; i++) {
     	vector[i] = i;
-    	pila_apilar(p1, &vector[i]);
-    	print_test("Se apilo correctamente", pila_ver_tope(p1) == &vector[i]);
+    	ok &= pila_apilar(p1, &vector[i]);
     }
+    print_test("Se apilaron todos los elementos", ok);
+
+    //pruebo desapilar:
+    ok = true;
+    for(int i = 0; i < volumen; i++) {
+    	ok &= pila_desapilar(p1);
+    }
+    print_test("Se desapilaron todos los elementos", ok);
 
     free(vector);
 
     pila_destruir(p1);
 
+}
 
+void pruebas_pila_alumno() {
 
-
-
-
+	prueba_pila_vacia();
+	prueba_pila_con_elementos();
+	pruebas_pila_volumen():
 
 }
